e862efab9e04e2898fc1f3cd7b34c19deeceae51
Mike Homayoun <morteza2002@gmail.com>
2025-04-16 16:12:23 -0400

    Fix icons
    Add build:docker:icons to gulp file to copy icons to the nodes/dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,25 @@ const path = require('path');
 const { task, src, dest } = require('gulp');
 
 task('build:icons', copyIcons);
+task('build:docker:icons', buildIconsForDocker);
 
-function copyIcons() {
+function copyIcons(dist = 'dist') {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-	const nodeDestination = path.resolve('dist', 'nodes');
+	const nodeDestination = path.resolve(dist, 'nodes');
 
 	src(nodeSource).pipe(dest(nodeDestination));
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-	const credDestination = path.resolve('dist', 'credentials');
+	const credDestination = path.resolve(dist, 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
+}
+
+function buildIconsForDocker() {
+	// read outDir from tsconfig.docker.json
+	const tsconfig = require('./tsconfig.docker.json');
+	const outDir = tsconfig.compilerOptions.outDir;
+
+	copyIcons(outDir);
+	return Promise.resolve();
 }
